89a2a6c653b30e2883a0fdb12f5aacddcbcd2763
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-12-29 15:26:16 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,32 @@
+
+//esconde o botão sair
+document.getElementById("botao3").style.display ='none';
+//crie a variavel idade
+
+function add(){
+    //aumente o valor de idade
+
+    
+    //exiba seu valor
+
+
+    //MOSTRAR O BOTÃO SALVAR E A CAIXA DE TEXTO
+    document.getElementById("botao2").style.display ='inline-block';
+    document.getElementById("nome").style.display ='inline-block';
+        
+}
+
+function salvar(){
+    
+    //guarda o nome na VARIÁVEL
+
+    //guardar os valores (nome e idade) na MEMÓRIA DO NAVEGADOR
+ 
+
+    document.getElementById("botao3").style.display ='inline-block';
+}
+
+function trocarPagina(){
+    // trocar de site 
+
+}
